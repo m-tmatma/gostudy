f72cee96425ee6c70de5d9eb6a58bff6edc5ebcb
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-05-02 19:10:05 +0900

    bool テスト

--- chap2/println.go
+++ chap2/println.go
@@ -7,4 +7,9 @@ func main() {
 	fmt.Println(len("Hello, World"))
 	fmt.Println("Hello! World"[1])
 	fmt.Println("Hello, " + "World")
+
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(!true)
 }
