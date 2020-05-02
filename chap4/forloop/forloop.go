d7d44ad1909551b3b96df54b193829394995cbc6
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-05-02 19:44:56 +0900

    switch

--- chap4/forloop/forloop.go
+++ chap4/forloop/forloop.go
@@ -13,4 +13,23 @@ func main() {
 	for j := 1; j <= 10; j++ {
 		fmt.Println(j)
 	}
+
+	for k := 1; k <= 10; k++ {
+		switch k {
+		case 0:
+			fmt.Println("Zero")
+		case 1:
+			fmt.Println("One")
+		case 2:
+			fmt.Println("Two")
+		case 3:
+			fmt.Println("Three")
+		case 4:
+			fmt.Println("Four")
+		case 5:
+			fmt.Println("Five")
+		default:
+			fmt.Println("Unknown")
+		}
+	}
 }
