5e43d05d96d87fa4a391cfcc292913eb052c108f
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-05-06 16:57:07 +0900

    range 2 行

--- chap5/map/map.go
+++ chap5/map/map.go
@@ -6,10 +6,21 @@ func main() {
 	//var x map[string]int
 	x := make(map[string]int)
 	x["key"] = 10
+	x["key2"] = 12
 
 	fmt.Println(x["key"])
 
+	for i, data := range x {
+		fmt.Println(i, data)
+	}
+
 	y := make(map[int]int)
 	y[1] = 10
+	y[10] = 20
+
 	fmt.Println(y[1])
+
+	for i, data := range y {
+		fmt.Println(i, data)
+	}
 }
