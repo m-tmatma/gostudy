676f5a463358969aefb202c1821c0222340c95f6
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-05-17 16:08:35 +0900

    crc32

--- chap8/crc32/crc32.go
+++ chap8/crc32/crc32.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 import "hash/crc32"
+import "io"
+import "os"
+
+func getHash(filename string) (uint32, error){
+	f, err := os.Open(filename)
+	if err != nil {
+		return 0, err
+	}
+	defer f.Close()
+
+	h := crc32.NewIEEE()
+	_, err = io.Copy(h, f)
+	if err != nil {
+		return 0, err
+	}
+
+	return h.Sum32(), nil
+}
 
 func main(){
 	h := crc32.NewIEEE()
@@ -10,4 +28,6 @@ func main(){
 
 	v := h.Sum32()
 	fmt.Println(v)
+
+	fmt.Println(getHash("crc32.go"))
 }
