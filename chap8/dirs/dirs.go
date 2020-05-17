29a4ad095e3607ce06277b0decd87dc1fa7da598
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-05-17 14:46:27 +0900

    filepath を追加

--- chap8/dirs/dirs.go
+++ chap8/dirs/dirs.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 func main() {
@@ -19,4 +20,9 @@ func main() {
 	for _, fi := range fileInfos {
 		fmt.Println(fi.Name())
 	}
+
+	filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+		fmt.Println(path)
+		return nil
+	})
 }
